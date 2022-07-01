a4544894b9061dd9c476af34061602bfca2c6e00
mann0777 <mannamloh77@gmail.com>
2022-07-02 02:03:25 +0530

    Added-header-footer

---
+++ src/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header=()=>{
+    return(
+    <>
+    <div className="header">
+    <h2>Note Keep</h2>
+    </div>
+        
+    </>
+    );
+
+};
+
+export default Header;
